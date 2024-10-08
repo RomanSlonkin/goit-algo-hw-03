3766d13ae98f932e33c8b5cf15dceaea12f7eaf8
romanslonkin <romanslonkin@gmail.com>
2024-10-08 20:25:53 +0300

    task2_2

--- homework_task2.py
+++ homework_task2.py
@@ -1,10 +1,13 @@
 from random import sample
 
-def get_numbers_ticket(min, max, quantity):
-    if min < 1 or max > 1000 or min > quantity > max or min>max:
-        print("Please, use valid values")
-    return (sorted(sample(range(min, max), quantity)))
+def get_numbers_ticket(min: int, max: int, quantity: int) -> int:
+    if min < 1 or max > 1000 or quantity > len(range(min, max)) or min>max:
+        while True:
+            print("Please, use valid values")
+            break
+    else:
+        return (sorted(sample(range(min, max), quantity)))
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(15, 1000, 30)
 print("Ваші лотерейні числа:", lottery_numbers)
 
