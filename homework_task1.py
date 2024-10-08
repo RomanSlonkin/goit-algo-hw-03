a510111913c13815997cb1c8d2f1af3b535433dc
romanslonkin <romanslonkin@gmail.com>
2024-10-08 21:19:31 +0300

    tasks

--- homework_task1.py
+++ homework_task1.py
@@ -1,9 +1,12 @@
+"""Module providing a function to work with date"""
 from datetime import datetime
 
 def get_days_from_today(date):
+    """function to count difference in date and todays date in days  """
     requsted_date = datetime.strptime(date, "%Y-%m-%d").date()
     date_today = datetime.today().date()
     days_from_today = date_today - requsted_date
     print(days_from_today.days)
 
-get_days_from_today("2026-10-09")
+get_days_from_today("2021-05-09")
+# End-of-file (EOF)
--- homework_task2.py
+++ homework_task2.py
@@ -1,13 +1,15 @@
+""" Module providing a function to get unique random values """
 from random import sample
 
 def get_numbers_ticket(min: int, max: int, quantity: int) -> int:
-    if min < 1 or max > 1000 or quantity > len(range(min, max)) or min>max:
+    """ function to get unique values from range """
+    if min < 1 or max > 1000 or quantity > len(range(min, max)):
         while True:
             print("Please, use valid values")
             break
     else:
         return (sorted(sample(range(min, max), quantity)))
 
-lottery_numbers = get_numbers_ticket(15, 1000, 30)
+lottery_numbers = get_numbers_ticket(100, 15, 10)
 print("Ваші лотерейні числа:", lottery_numbers)
-
+# End-of-file (EOF)
